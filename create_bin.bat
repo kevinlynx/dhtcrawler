d1e8a154d86d1f1b8f0980ab7246b05bb2834de3
Kevin Lynx <kevinlynx@gmail.com>
2013-06-27 12:42:16 +0800

    fix the path

--- create_bin.bat
+++ create_bin.bat
@@ -1,10 +1,10 @@
 cd ..
-mkdir bin\deps\bson
-mkdir bin\deps\mongodb
-mkdir bin\deps\kdht
-copy deps\bson\ebin\*.* bin\deps\bson\
-copy deps\mongodb\ebin\*.* bin\deps\mongodb\
-copy deps\kdht\ebin\*.* bin\deps\kdht\
+mkdir bin\deps\bson\ebin
+mkdir bin\deps\mongodb\ebin
+mkdir bin\deps\kdht\ebin
+copy deps\bson\ebin\*.* bin\deps\bson\ebin\
+copy deps\mongodb\ebin\*.* bin\deps\mongodb\ebin\
+copy deps\kdht\ebin\*.* bin\deps\kdht\ebin\
 mkdir bin\www
 copy www\*.* bin\www\
 copy tools\*.* bin\
